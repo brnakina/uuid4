2fe8ddec711358922e0cdb17e46837bc288efdbd
Ubuntu <ubuntu@ip-172-31-38-20.us-east-2.compute.internal>
2017-07-05 14:52:34 +0900

    add version, variant.

--- uuid4.js
+++ uuid4.js
@@ -1,7 +1,7 @@
 function uuid4(){
 	var i, len, j, lenj, pointer = 0, random = [], block = "", blocks = [], uuidFormat = [8,4,4,4,12];
 	for(i = 0; i < 32; i += 1){
-		random.push(parseInt(Math.random() * 16, 10).toString(16));
+		random.push(getRandom(i));
 	}
 	for(i = 0, len = uuidFormat.length; i < len; i += 1){
 		block = "";
@@ -11,4 +11,20 @@ function uuid4(){
 		blocks.push(block);
 	}
 	return blocks.join('-');
+
+	function getRandom(i){
+		var ret;
+		switch(i){
+			case 12:
+			ret = 4;
+			break;
+			case 16:
+			ret = parseInt(Math.random() * 4 + 8, 10).toString(16);
+			break;
+			default:
+			ret = parseInt(Math.random() * 16, 10).toString(16);
+			break; 
+		}
+		return ret;
+	}
 }
